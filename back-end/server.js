b5d1c1eafda118b3e100fc75a6e07d76d8e9a7b2
Angelo Bovino <angelo.bovino@outlook.com>
2016-10-08 00:17:13 -0400

    added some comments and moved stuff to services/controllers folders

--- back-end/server.js
+++ back-end/server.js
@@ -3,30 +3,26 @@ var app = express();
 var bodyParser = require('body-parser');
 //var mongo = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
+var jwt = require('jwt-simple');
+var moment = require('moment');
 
 var auth = require('./controllers/auth');
 var message = require('./controllers/message');
+var checkAuthenticated = require('./services/checkAuthenticated');
+var cors = require('./services/cors');
 
+//Middleware
 app.use(bodyParser.json());
+app.use(cors);
 
-app.use(function(req,res,next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
+//Requests
 app.get('/api/message', message.get);
 
-app.post('/api/message', message.post);
+app.post('/api/message', checkAuthenticated, message.post);
 
 app.post('/auth/register', auth.register);
 
-function GetMessages(req, res) {
-  Message.find({}).exec(function(err, result) {
-    res.send(result);
-  });
-}
-
+//Database Connection
 mongoose.connect("mongodb://localhost:27017/test", function(err,db) {
   if(!err) {
     console.log("Connected to Mongo");
