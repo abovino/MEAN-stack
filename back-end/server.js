254ffb9dfa872438dae4fa46068ad8fb61a87f61
Angelo Bovino <angelo.bovino@outlook.com>
2016-10-07 23:13:55 -0400

    removed all of the route callbacks to controllers folder to clean up file, and required them from controllers folder

--- back-end/server.js
+++ back-end/server.js
@@ -4,10 +4,8 @@ var bodyParser = require('body-parser');
 //var mongo = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 
-var database;
-var Message = mongoose.model('Message', {
-  msg: String
-});
+var auth = require('./controllers/auth');
+var message = require('./controllers/message');
 
 app.use(bodyParser.json());
 
@@ -17,18 +15,11 @@ app.use(function(req,res,next) {
   next();
 });
 
-app.get('/api/message', GetMessages);
+app.get('/api/message', message.get);
 
-app.post('/api/message', function(req, res) {
-  console.log(req.body);
-  var message = new Message(req.body);
-  message.save();
-  res.status(200);
-});
+app.post('/api/message', message.post);
 
-app.post('/auth/register', function(req, res) {
-  console.log(req.body);
-})
+app.post('/auth/register', auth.register);
 
 function GetMessages(req, res) {
   Message.find({}).exec(function(err, result) {
